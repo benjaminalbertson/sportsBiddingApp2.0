da32daa019540054053e8f1069a9112faf799d86
Benjamin Albertson <benalbertson1@hotmail.com>
2019-04-28 16:42:21 -0500

    user page shows bets based on the user id

--- sportsBiddingApp2.0/Default.aspx.designer.cs
+++ sportsBiddingApp2.0/Default.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace sportsBiddingApp2._0 {
     
     
     public partial class _Default {
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
